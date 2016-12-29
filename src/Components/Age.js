90fb16160a2af4183348af3c3ac2d9ab43c02e2b
Kelvan Ince <incekelvan@gmail.com>
2016-12-29 15:49:01 -0600

    Removed pictures from project

--- src/Components/Age.js
+++ src/Components/Age.js
@@ -9,10 +9,8 @@ class Age extends Component {
         <p>With an average age of just 25</p>
         <h2>Whereas the Falcons have the oldest</h2>
         <p>Averaging at 27.08</p>
-        // <img src="./FalconsAge.jpg" />
         <h2>When you consider how young the Seahawks are,<br /> their results over the last several years is even <br /> more impressive</h2>
         <p>Their average age is just 25.6</p>
-        // <img src="./SeahawksAge.jpg" />
       </div>
     );
   }
